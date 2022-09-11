42e37294c965ab303ea4affdec7c718bf8306665
Ashraff0712 <Ashraff*7416>
2022-09-11 13:04:20 +0530

    New change Day3complete

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 
@@ -10,12 +11,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragmentManager: FragmentManager = supportFragmentManager
-        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-
-        val signInFragment = SignInFragment()
-
-        fragmentTransaction.add(R.id.signIn,signInFragment)
-        fragmentTransaction.commit()
+//        val fragmentManager: FragmentManager = supportFragmentManager
+//        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+//
+//        val signInFragment = SignInFragment()
+//
+//        fragmentTransaction.add(R.id.signIn,signInFragment)
+//        fragmentTransaction.commit()
+        Log.d("Message", "Main Activity onCreate")
     }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Message", "Main Activity onDestroy")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Message", "Main Activity onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Message", "Main Activity onStop")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Message", "Main Activity onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Message", "Main Activity onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("Message", "Main Activity onRestart")
+    }
+
+
 }
